ed761f9c8ca5ee7d390b164ac0272dea0fa7f23c
Emmanuel Anzele <eanzele@gmail.com>
2019-01-02 21:20:37 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,3 +2,7 @@
 def display_rainbow(colors)
   puts #{colors[0] [1] [2] [3] [4] [5] [6]}
 end
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]} , Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I:#{colors[5]}, V: #{colors[6]}"
+end
